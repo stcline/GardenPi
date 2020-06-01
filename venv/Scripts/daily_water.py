ad3e106b7657ee0b9128cfe21b006f6e7b0253da
stcline <steve.cline@weldre4.org>
2020-06-01 11:48:41 -0600

    add daily_water.py script

---
+++ venv/Scripts/daily_water.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from gpiozero import LED
+from time import sleep
+import datetime
+
+start = ["06","30")
+
+solenoid = LED(5)
+yelLED = LED(27)
+mins = 10
+
+sleep (60)
+
+for x in range(4):
+    yelLED.on()
+    sleep (.3)
+    yelLED.off()
+    sleep(.3)
+
+while True:
+    now = datetime.datetime.now()
+    if now.strftime("%H") == start[0] and now.strftime("%M") == start[1] and now.strftime("%S") == "00":
+        print ("Time to water!")
+        yelLED.on()
+        solenoid.on()
+        sleep(60*mins)
+        print ("All done")
+        yelLED.off()
+        solenoid.off()
