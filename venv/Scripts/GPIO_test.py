874e1c55d788281ad1b489235395a7f0a30c3c8d
stcline <steve.cline@weldre4.org>
2021-01-28 20:06:17 -0700

    Update GPIO_test.py

--- venv/Scripts/GPIO_test.py
+++ venv/Scripts/GPIO_test.py
@@ -1,6 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+switchPin = 8
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(switchPin, GPIO.OUT, initial=GPIO.LOW)
+
+# Run a quick test of the switch
+print ("The switch is on")
+GPIO.output(switchPin, GPIO.HIGH)
+sleep (2)
+print ("The switch is off")
+GPIO.output(switchPin, GPIO.LOW)
